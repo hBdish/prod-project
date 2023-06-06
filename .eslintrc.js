e2a27e39a53e66a294c22282050f22e795e3406f
maksim <maksim3602@gmail.com>
2023-06-06 19:53:49 +0300

    debugged

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,17 +28,37 @@ module.exports = {
     'react/require-default-props': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-props-no-spreading': 'warn',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: [
+          '.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
     'react/function-component-definition': 'off',
     'import/no-extraneous-dependencies': 'warn',
     'import/extensions': 'off',
     'no-shadow': 'off',
     'no-unused-vars': 'warn',
     'no-underscore-dangle': 'off',
-    'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'i18next/no-literal-string': [
+      'error',
+      {
+        markupOnly: true,
+        ignoreAttribute: ['data-testid'],
+      },
+    ],
     'max-len': ['error', { ignoreComments: true, code: 100 }],
   },
   globals: {
     __IS_DEV__: true,
   },
+  overrides: [
+    {
+      files: ['**/src/**/*.test.{ts,tsx}'],
+      rules: {
+        'i18next/no-literal-string': 'off',
+      },
+    },
+  ],
 };

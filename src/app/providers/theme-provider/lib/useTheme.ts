c4ba8a86ab70debed27ae275c6cebbdc51447f53
maksim <maksim3602@gmail.com>
2023-06-11 15:07:28 +0300

    fixed theme switcher

--- src/app/providers/theme-provider/lib/useTheme.ts
+++ src/app/providers/theme-provider/lib/useTheme.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './theme.context';
 
 interface UseThemeResult {
@@ -9,10 +9,13 @@ interface UseThemeResult {
 export function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   const toggleTheme = () => {
     const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
     setTheme(newTheme);
-    document.body.className = newTheme;
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 

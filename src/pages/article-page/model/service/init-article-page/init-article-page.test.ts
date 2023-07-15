b561d270690f1ba564100541abca9d3adb017608
maksim <maksim3602@gmail.com>
2023-07-15 11:57:01 +0300

    added test

---
+++ src/pages/article-page/model/service/init-article-page/init-article-page.test.ts
@@ -0,0 +1,49 @@
+import TestAsyncThunk from 'shared/lib/test/test-async-thunk';
+import { initArticlePage } from './init-article-page';
+import { fetchArticlesList } from '../fetch-articles-list/fetch-articles-list';
+
+jest.mock('../fetch-articles-list/fetch-articles-list');
+
+describe('initArticlePage.test', () => {
+  test('not inited', async () => {
+    const thunk = new TestAsyncThunk(initArticlePage, {
+      articlesPage: {
+        page: 1,
+        ids: [],
+        entities: {
+
+        },
+        limit: 5,
+        isLoading: false,
+        hasMore: true,
+        _inited: false,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toBeCalledTimes(4);
+    expect(fetchArticlesList).toBeCalledTimes(1);
+  });
+
+  test('inited', async () => {
+    const thunk = new TestAsyncThunk(initArticlePage, {
+      articlesPage: {
+        page: 1,
+        ids: [],
+        entities: {
+
+        },
+        limit: 5,
+        isLoading: false,
+        hasMore: true,
+        _inited: true,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toBeCalledTimes(2);
+    expect(fetchArticlesList).toBeCalledTimes(0);
+  });
+});

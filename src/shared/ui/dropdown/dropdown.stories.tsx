fdb601948d183abb4ce007cb49ebab23b2e9af07
maksim <maksim3602@gmail.com>
2023-07-24 21:16:27 +0300

    add stories

---
+++ src/shared/ui/dropdown/dropdown.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Dropdown } from './dropdown';
+
+const meta: Meta<typeof Dropdown> = {
+  title: 'shared/Dropdown',
+  component: Dropdown,
+  args: {},
+  argTypes: {},
+
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Dropdown>;
+
+const MOCK = 'TEST';
+export const Primary: Story = {
+  args: {
+    trigger: <button type="button">{MOCK}</button>,
+    direction: 'bottomRight',
+    items: [{
+      content: 'Test1',
+    }],
+  },
+};

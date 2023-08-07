734b51b00f7dd352323838b04a831f212f01492e
Maksim Doronkin <maksim3602@gmail.com>
2023-08-07 14:00:02 +0300

    add theme addon for storybook

--- config/.storybook/preview.ts
+++ config/.storybook/preview.ts
@@ -1,10 +1,19 @@
 import type {Preview} from "@storybook/react";
-import {RouterDecorator, StyleDecorator, ThemeDecorator} from "../../src/shared/config/storybook";
+import {RouterDecorator, StyleDecorator} from "../../src/shared/config/storybook";
 
 import {LanguageDecorator} from "../../src/shared/config/storybook/language-decorator";
 
 import {Theme} from "../../src/shared";
 
+/*export const parameters = {
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'light', class: ['app', Theme.LIGHT], color: '#e8b7b7' },
+      { name: 'dark', class: ['app', Theme.DARK], color: '#de0956' }
+    ],
+  },
+};*/
 
 const preview: Preview = {
   parameters: {
@@ -15,6 +24,13 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    themes: {
+      default: 'light',
+      list: [
+        { name: 'light', class: ['app', Theme.LIGHT], color: '#e8b7b7' },
+        { name: 'dark', class: ['app', Theme.DARK], color: '#021146' }
+      ],
+    },
   },
   decorators: [
     // @ts-ignore
@@ -22,11 +38,12 @@ const preview: Preview = {
     LanguageDecorator,
     // @ts-ignore
     StyleDecorator,
-    ThemeDecorator(Theme.LIGHT), // Theme on all stories
+    // ThemeDecorator(Theme.LIGHT), // Theme on all stories
     // @ts-ignore
     RouterDecorator,
   ]
 };
+
 
 
 export default preview;

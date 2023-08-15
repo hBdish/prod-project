20c5db6e6d8635d23575bee4f855b29e7a93909e
Maksim Doronkin <maksim3602@gmail.com>
2023-08-15 12:32:28 +0300

    add bash script for deploy

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -3,4 +3,4 @@ npm run build:prod
 
 rm -rf ~/../var/www/prod-project/html
 # shellcheck disable=SC2224
-mv prod-project/build ~/../var/www/prod-project/html
+mv ~/../prod-project/build ~/../var/www/prod-project/html
